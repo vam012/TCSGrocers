1f2263f80fb4fd98fbe7a19fd4983515436405c5
Vincent Minnocci <vincentminnocci@gmail.com>
2021-04-22 14:51:51 -0400

    created admin login page and imported formsmodule

--- Frontend/tcs-grocers/src/app/admin-login/admin-login.component.ts
+++ Frontend/tcs-grocers/src/app/admin-login/admin-login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-login',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminLoginComponent implements OnInit {
 
-  constructor() { }
+  msg?:String
+  constructor(public router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  loginAttempt(loginRef:any):void{
+    this.msg = `username ${loginRef.username}, pwd ${loginRef.password}`;
+
   }
 
 }
--- Frontend/tcs-grocers/src/app/app.module.ts
+++ Frontend/tcs-grocers/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminHomeComponent } from './admin-home/admin-home.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AdminLoginComponent } from './admin-login/admin-login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
