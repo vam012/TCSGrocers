36679ef0256c6144c60996a9ac4a49eb3c9cb634
Vincent Minnocci <vincentminnocci@gmail.com>
2021-04-22 15:28:51 -0400

    added admin update product info template

--- Frontend/tcs-grocers/src/app/admin-update-product/admin-update-product.component.ts
+++ Frontend/tcs-grocers/src/app/admin-update-product/admin-update-product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-update-product',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminUpdateProductComponent implements OnInit {
 
-  constructor() { }
+  product={
+    name:"Product Name",
+    price:10.00,
+    quantity:100,
+    discount:0
+  }
+  msg?:string
+  selectedProductID?:any
+  constructor(public router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  goToAdminMenu():void{
+    this.msg = "go back to admin menu"
+  }
+
+  updateProductID(formRef:any):void{
+    this.selectedProductID = formRef.productID;
+  }
+
+  updateProductName(formRef:any):void{
+    this.product.name = formRef.productName;
+  }
+
+  updateProductPrice(formRef:any):void{
+    this.product.price = formRef.productPrice;
+  }
+
+  updateProductQuantity(formRef:any):void{
+    this.product.quantity = formRef.productQuantity;
+  }
+
+  updateProductDiscount(formRef:any):void{
+    this.product.discount = formRef.productDiscount;
   }
 
 }
