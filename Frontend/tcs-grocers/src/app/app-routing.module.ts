b4647711a837212f7c7f44c267a8a04b7c603314
Vincent Minnocci <vincentminnocci@gmail.com>
2021-04-22 10:54:39 -0400

    added admin login page with routing

--- Frontend/tcs-grocers/src/app/app-routing.module.ts
+++ Frontend/tcs-grocers/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminHomeComponent } from './admin-home/admin-home.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
 
 const routes: Routes = [
-  {path:'\admin',component:AdminHomeComponent}
+  {path:'\admin',component:AdminHomeComponent},
+  {path:`\adminlogin`,component:AdminLoginComponent}
 ];
 
 @NgModule({
--- Frontend/tcs-grocers/src/app/app.module.ts
+++ Frontend/tcs-grocers/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminHomeComponent } from './admin-home/admin-home.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminHomeComponent
+    AdminHomeComponent,
+    AdminLoginComponent
   ],
   imports: [
     BrowserModule,
