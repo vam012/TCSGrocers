39029c28ca6d9dd96c1720e2fa30762a4af361ed
Rudrasaitej Yepuri <Yepuri.Rudra@outlook.com>
2021-04-22 20:47:22 -0500

    created add employee component and wrote template code.

--- Frontend/tcs-grocers/src/app/app-routing.module.ts
+++ Frontend/tcs-grocers/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminAddEmployeeComponent } from './admin-add-employee/admin-add-employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'\adminaddemployee', component:AdminAddEmployeeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/tcs-grocers/src/app/app.module.ts
+++ Frontend/tcs-grocers/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminAddEmployeeComponent } from './admin-add-employee/admin-add-employee.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminAddEmployeeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
