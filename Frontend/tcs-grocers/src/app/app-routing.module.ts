c12ff08afc6b46c1179ebb9ba8a6e1d894a6a84e
Vincent Minnocci <vincentminnocci@gmail.com>
2021-04-23 15:07:06 -0400

    removed old imports

--- Frontend/tcs-grocers/src/app/app-routing.module.ts
+++ Frontend/tcs-grocers/src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AdminAddEmployeeComponent } from './admin-add-employee/admin-add-employee.component';
-import { AdminDeleteEmployeeComponent } from './admin-delete-employee/admin-delete-employee.component';
 import { AdminHomeComponent } from './admin-home/admin-home.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { AdminMenuComponent } from './admin-menu/admin-menu.component';
