00d39ed7158184678ccaa4b1cebc91e4b445cae0
Rudrasaitej Yepuri <Yepuri.Rudra@outlook.com>
2021-04-22 21:10:02 -0500

    created admin delete employee component, wrote template code and added path.

--- Frontend/tcs-grocers/src/app/app-routing.module.ts
+++ Frontend/tcs-grocers/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminAddEmployeeComponent } from './admin-add-employee/admin-add-employee.component';
+import { AdminDeleteEmployeeComponent } from './admin-delete-employee/admin-delete-employee.component';
 
 const routes: Routes = [
-  {path:'\adminaddemployee', component:AdminAddEmployeeComponent}
+  {path:'\adminaddemployee', component:AdminAddEmployeeComponent},
+  {path:'\admindeleteemployee', component:AdminDeleteEmployeeComponent}
 ];
 
 @NgModule({
--- Frontend/tcs-grocers/src/app/app.module.ts
+++ Frontend/tcs-grocers/src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AdminAddEmployeeComponent } from './admin-add-employee/admin-add-employee.component';
 import { FormsModule } from '@angular/forms';
+import { AdminAddEmployeeComponent } from './admin-add-employee/admin-add-employee.component';
+import { AdminDeleteEmployeeComponent } from './admin-delete-employee/admin-delete-employee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminAddEmployeeComponent,
+    AdminDeleteEmployeeComponent,
   ],
   imports: [
     BrowserModule,
