a3de6369439d7a637f0adc1cca55978e1c0219f1
Vincent Minnocci <vincentminnocci@gmail.com>
2021-04-23 13:03:17 -0400

    remove unused paths from routing component

--- Frontend/tcs-grocers/src/app/app-routing.module.ts
+++ Frontend/tcs-grocers/src/app/app-routing.module.ts
@@ -1,21 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AdminAddProductComponent } from './admin-add-product/admin-add-product.component';
-import { AdminDeleteProductComponent } from './admin-delete-product/admin-delete-product.component';
 import { AdminHomeComponent } from './admin-home/admin-home.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { AdminMenuComponent } from './admin-menu/admin-menu.component';
-import { AdminUpdateProductComponent } from './admin-update-product/admin-update-product.component';
-import { AdminViewRequestsComponent } from './admin-view-requests/admin-view-requests.component';
 
 const routes: Routes = [
   {path:'\admin',component:AdminHomeComponent},
   {path:`\adminlogin`,component:AdminLoginComponent},
-  {path:`\adminmenu`,component:AdminMenuComponent},
-  {path:`\adminaddproduct`,component:AdminAddProductComponent},
-  {path:`\admindeleteproduct`,component:AdminDeleteProductComponent},
-  {path:`\adminupdateproduct`,component:AdminUpdateProductComponent},
-  {path:`\adminviewrequests`,component:AdminViewRequestsComponent}
+  {path:`\adminmenu`,component:AdminMenuComponent}
 ];
 
 @NgModule({
