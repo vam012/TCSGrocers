7b12e22a3563e4160472988227841f930f4d3c95
Vincent Minnocci <vincentminnocci@gmail.com>
2021-04-26 15:34:02 -0400

    fixed form submit reset

--- Frontend/tcs-grocers/src/app/admin-add-product/admin-add-product.component.ts
+++ Frontend/tcs-grocers/src/app/admin-add-product/admin-add-product.component.ts
@@ -16,7 +16,9 @@ export class AdminAddProductComponent implements OnInit {
   }
 
   addNewProduct(productRef:any):void{
-    this.prodServ.addNewProduct(productRef).subscribe((res:string)=>this.msg=res)
+    this.prodServ.addNewProduct(productRef.value).subscribe((res:string)=>{
+      this.msg=res;
+    productRef.reset()})
   }
 
 }
