59f66f5afc737e17aa9033eea26a0879d524f854
Vincent Minnocci <vincentminnocci@gmail.com>
2021-04-26 17:32:04 -0400

    added route and redirect path

--- Frontend/tcs-grocers/src/app/app-routing.module.ts
+++ Frontend/tcs-grocers/src/app/app-routing.module.ts
@@ -4,11 +4,14 @@ import { AdminAuthGaurdService } from './admin-auth-gaurd.service';
 import { AdminHomeComponent } from './admin-home/admin-home.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { AdminMenuComponent } from './admin-menu/admin-menu.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
 const routes: Routes = [
+  {path:`\mainmenu`,component:MainMenuComponent},
   {path:'\admin',component:AdminHomeComponent},
   {path:`\adminlogin`,component:AdminLoginComponent},
-  {path:`\adminmenu`,component:AdminMenuComponent,canActivate:[AdminAuthGaurdService]}
+  {path:`\adminmenu`,component:AdminMenuComponent,canActivate:[AdminAuthGaurdService]},
+  {path:``,redirectTo:"\mainmenu",pathMatch:"full"},
 ];
 
 @NgModule({
