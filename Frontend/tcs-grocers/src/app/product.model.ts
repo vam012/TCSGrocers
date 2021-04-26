d7a9ef8a90910b2eedf68fb76b8254f9b786f75a
Vincent Minnocci <vincentminnocci@gmail.com>
2021-04-26 10:54:31 -0400

    delete product completed

---
+++ Frontend/tcs-grocers/src/app/product.model.ts
@@ -0,0 +1,7 @@
+export class Product{
+    constructor(public _id:number,
+        public name:string,
+        public quantity:number,
+        public price:number,
+        public discount:number){}
+}
