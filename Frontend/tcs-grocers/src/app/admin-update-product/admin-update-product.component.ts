461a11f4a653b157f67ed17e51d58b7a1384e760
Vincent Minnocci <vincentminnocci@gmail.com>
2021-04-27 11:07:08 -0400

    added form validation on update product quantity

--- Frontend/tcs-grocers/src/app/admin-update-product/admin-update-product.component.ts
+++ Frontend/tcs-grocers/src/app/admin-update-product/admin-update-product.component.ts
@@ -15,6 +15,7 @@ export class AdminUpdateProductComponent implements OnInit {
   selectedProductID?:any;
   productPriceErrMsg?:string;
   productDiscountErrMsg?:string;
+  productQuantityErrMsg?: string;
   constructor(public router:Router,public prodServ:ProductService) { }
 
   ngOnInit(): void {
@@ -43,7 +44,7 @@ export class AdminUpdateProductComponent implements OnInit {
   }
 
   updateProductPrice(formRef:any):void{
-    if(!(formRef.value.productPrice < 0)){
+    if(formRef.value.productPrice > 0){
       this.prodServ.updateProductPrice({'productID':this.selectedProductID,'newPrice':formRef.value.productPrice}).subscribe((res:string)=>{
         formRef.reset();
         this.productPriceErrMsg = ""
@@ -56,15 +57,20 @@ export class AdminUpdateProductComponent implements OnInit {
   }
 
   updateProductQuantity(formRef:any):void{
-    this.prodServ.updateProductQuantity({'productID':this.selectedProductID,'newQuantity':formRef.value.productQuantity}).subscribe((res:string)=>{
-      formRef.reset();
-      this.getProducts();
-      this.getUpdatedProduct();
+    if(formRef.value.productQuantity>0){
+      this.prodServ.updateProductQuantity({'productID':this.selectedProductID,'newQuantity':formRef.value.productQuantity}).subscribe((res:string)=>{
+        formRef.reset();
+        this.productQuantityErrMsg = ""
+        this.getProducts();
+        this.getUpdatedProduct();
     })
+    }else{
+      this.productQuantityErrMsg = "Please input a positive product quantity!"
+    }
   }
 
   updateProductDiscount(formRef:any):void{
-    if(!(formRef.value.productDiscount<0 || formRef.value.productDiscount >100)){
+    if(formRef.value.productDiscount>0 && formRef.value.productDiscount <100){
     this.prodServ.updateProductDiscount({'productID':this.selectedProductID,'newDiscount':formRef.value.productDiscount}).subscribe((res:string)=>{
       this.productDiscountErrMsg = ""
       formRef.reset();
